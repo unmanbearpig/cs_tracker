653f5c1f58ce12c91db13dbc48934ea18aa2cbba
unmanbearpig <unmanbearpig@gmail.com>
2014-03-17 17:57:25 +0400

    Added hstore

---
+++ db/migrate/20140317135112_add_hstore.rb
@@ -0,0 +1,9 @@
+class AddHstore < ActiveRecord::Migration
+  def up
+    execute 'create extension hstore'
+  end
+
+  def down
+    execute 'drop extension hstore'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140317135112) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "hstore"
 
 end
