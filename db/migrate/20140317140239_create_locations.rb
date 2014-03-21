9f010ca1dae246b9f55abcf4f8d88c477fd6bd51
unmanbearpig <unmanbearpig@gmail.com>
2014-03-21 19:07:45 +0400

    Added not null constraint for location city_id

--- db/migrate/20140317140239_create_locations.rb
+++ db/migrate/20140317140239_create_locations.rb
@@ -1,7 +1,7 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-      t.string :city_id
+      t.string :city_id, null: false
       t.hstore :data
 
       t.timestamps
