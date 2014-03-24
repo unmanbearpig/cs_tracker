830b81dab1c8f448b05ab4c9d25eab53c72b80d3
unmanbearpig <unmanbearpig@gmail.com>
2014-03-24 12:29:26 +0400

    Added search queries to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :search_queries, through: :user_search_queries
 end
