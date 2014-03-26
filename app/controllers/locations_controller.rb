28958359da14008f0964654cf4b542092df5608b
unmanbearpig <unmanbearpig@gmail.com>
2014-03-27 00:34:13 +0400

    refactored locations#search a bit

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,19 +3,7 @@ class LocationsController < ApplicationController
     query = params[:q]
     return render nothing: true, status: 400 if query.nil? || query.empty?
 
-    location_ids = []
-
-    bg_status = Location.background_fetch(query)
-
-    if bg_status == :completed
-      location_ids += Location.background_fetch_results query
-    end
-
-    location_ids += Location.megasearch query || []
-
-    locations = fetch_locations location_ids
-
-    results = { status: bg_status, locations: locations }
+    results = { status: task_status(query), locations: search_locations(query) }
 
     respond_to do |format|
       format.json { render json: results }
@@ -24,9 +12,36 @@ class LocationsController < ApplicationController
 
   private
 
-  def fetch_locations locations
-    return [] unless locations || locations.empty?
+  def task_status query
+    Location.background_fetch(query)
+  end
+
+  def search_locations query
+    local_search_location_ids = background_task_location_ids(query) + prev_queries_location_ids(query)
+    local_search_results = fetch_locations(local_search_location_ids)
+
+    (local_search_results + full_text_search_results(query)).uniq
+  end
+
+  def full_text_search_results query
+    Location.search query
+  end
+
+  def prev_queries_location_ids query
+    Location.search_previous_queries query || []
+  end
+
+  def background_task_location_ids query
+    query_location_ids = []
+    if task_status(query) == :completed
+      query_location_ids = Location.background_fetch_results query
+    end
+    query_location_ids
+  end
+
+  def fetch_locations location_ids
+    return [] unless location_ids || location_ids.empty?
     Location.select(%i(id city state country))
-      .find(locations.reject(&:nil?))
+      .find(location_ids.reject(&:nil?))
   end
 end
