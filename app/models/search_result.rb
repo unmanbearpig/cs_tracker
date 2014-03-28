17425bd3cbbddbfd6a08fd64326ba2a8c5a48659
unmanbearpig <unmanbearpig@gmail.com>
2014-03-28 22:05:05 +0400

    search result validity timeout

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -4,6 +4,8 @@ class SearchResult < ActiveRecord::Base
 
   validates :search_query, presence: true
 
+  VALID_FOR = 4.hours
+
   def self.fetch cs, search_query, number_of_items = nil
     return unless search_query.kind_of? SearchQuery
 
