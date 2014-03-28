7c309f07f2e9aa4262f9763b7d61023324bd7b98
unmanbearpig <unmanbearpig@gmail.com>
2014-03-28 22:05:26 +0400

    typos

--- app/helpers/search_queries_helper.rb
+++ app/helpers/search_queries_helper.rb
@@ -2,8 +2,8 @@ module SearchQueriesHelper
   def humanized_search_mode
     case @search_mode.upcase
     when 'H' then 'hosts'
-    when 'S' then 'hurfers'
-    when 'L' then 'hocals'
+    when 'S' then 'surfers'
+    when 'L' then 'locals'
     when 'T' then 'travelers'
     else 'unknown'
     end
