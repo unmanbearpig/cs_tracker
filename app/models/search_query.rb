2c89e802e60b6ad985c18ef5eda00475f16ceac5
unmanbearpig <unmanbearpig@gmail.com>
2014-03-28 22:00:16 +0400

    search query update methods

--- app/models/search_query.rb
+++ app/models/search_query.rb
@@ -4,7 +4,31 @@ class SearchQuery < ActiveRecord::Base
 
   validates :location_id, uniqueness: { scope: [ :location_id, :search_mode ] }
 
+  before_save :sanitize_search_mode
+
+  def update_results
+    if up_to_date?
+      return :up_to_date if job.completed?
+      # return job.status unless job.failed?
+    end
+
+    update_results!
+  end
+
+  def update_results!
+    status = SearchQueryFetcher.status id
+
+    return status if status == :scheduled || status == :running
+
+    SearchQueryFetcher.enqueue(id).status
+  end
+
+  def job
+    SearchQueryFetcher.job id
+  end
+
   def last_items
+    return nil unless last_result
     last_result.search_items
   end
 
@@ -13,12 +37,19 @@ class SearchQuery < ActiveRecord::Base
       .order(created_at: :desc).first
   end
 
+  def up_to_date?
+    result = last_result
+    return false unless result
+
+    Time.now - result.created_at <= SearchResult::VALID_FOR
+  end
+
   def search(cs, number_of_items = nil)
     create_cs_search_query(cs).get(number_of_items)
   end
 
   def create_cs_search_query(cs)
-    cs.search.where(search_params)
+    cs.search.location(location.data).search_mode(search_mode)
   end
 
   def search_params
@@ -26,6 +57,10 @@ class SearchQuery < ActiveRecord::Base
       'location' => location.to_h,
       'search_mode' => search_mode
     }
+  end
+
+  def sanitize_search_mode
+    search_mode = search_mode.upcase
   end
 
 end
