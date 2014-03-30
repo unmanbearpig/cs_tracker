8af0185ea8449bbd7bcaecfaf862978f2a24d432
unmanbearpig <unmanbearpig@gmail.com>
2014-03-31 03:05:40 +0400

    implemented user_search_queries#create action

--- app/controllers/user_search_queries_controller.rb
+++ app/controllers/user_search_queries_controller.rb
@@ -14,11 +14,23 @@ class UserSearchQueriesController < ApplicationController
   end
 
   def create
+    search_query = SearchQuery.where(search_query_params).first_or_create
+    unless search_query
+      flash.now[:error] = 'Could not create a watch'
+      return redirect_to action: :new
+    end
+    user_search_query = UserSearchQuery.create search_query: search_query, user: current_user
+    redirect_to user_search_query
   end
 
   private
 
   def search_queries
     current_user.search_queries
+  end
+
+  def search_query_params
+    params.require(:location_id)
+    params.require(:search_mode)
   end
 end
