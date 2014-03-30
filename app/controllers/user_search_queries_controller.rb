eaffb28148e26c41fcc2246a147689a542b230e5
unmanbearpig <unmanbearpig@gmail.com>
2014-03-30 22:07:41 +0400

    redirect to new query page if user has none

--- app/controllers/user_search_queries_controller.rb
+++ app/controllers/user_search_queries_controller.rb
@@ -4,6 +4,7 @@ class UserSearchQueriesController < ApplicationController
   layout 'main'
 
   def index
+    return redirect_to new_user_search_query_path if search_queries.empty?
   end
 
   def show
@@ -13,5 +14,11 @@ class UserSearchQueriesController < ApplicationController
   end
 
   def create
+  end
+
+  private
+
+  def search_queries
+    current_user.search_queries
   end
 end
