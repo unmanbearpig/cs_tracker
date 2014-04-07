11de453ca16fe84e129d205994aa21853eacac40
unmanbearpig <unmanbearpig@gmail.com>
2014-04-08 02:18:21 +0400

    require authorisation for sidekiq web, add admin user later

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ CsTracker::Application.routes.draw do
     get 'search'
   end
 
-  mount Sidekiq::Web => '/sidekiq'
+  authenticate :user do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
