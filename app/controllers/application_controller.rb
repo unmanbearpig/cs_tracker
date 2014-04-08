8fd6ed4b524137c792616821b440d6b7b17ef479
unmanbearpig <unmanbearpig@gmail.com>
2014-04-08 17:44:28 +0400

    Added devise layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout :layout_by_resource
+
   def render_404
     raise ActionController::RoutingError.new('Not Found')
   end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise_layout"
+    else
+      "application"
+    end
+  end
+
 end
