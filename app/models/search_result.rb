dafb04395d49cee77d1e16edf15637be4613eb15
unmanbearpig <unmanbearpig@gmail.com>
2014-04-13 04:53:46 +0400

    fetch only search items for this query

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -29,9 +29,11 @@ class SearchResult < ActiveRecord::Base
   def items_by_first_appearance
     profile_ids = search_items.pluck(:profile_id)
     items = SearchItem
-      .select('distinct on (profile_id) *')
+      .select('distinct on (search_items.profile_id) *')
       .where('profile_id in (?)', profile_ids)
       .order(profile_id: :desc, created_at: :asc)
+      .joins(:search_result)
+      .where(search_results: {search_query_id: search_query_id})
       .to_a
       .sort { |item1, item2| item2.created_at <=> item1.created_at }
   end
