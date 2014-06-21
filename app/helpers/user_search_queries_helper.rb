bf9e0a0c67f23ed58468cf817d3a3a31adad4c45
unmanbearpig <unmanbearpig@gmail.com>
2014-06-21 04:32:42 +0400

    date format

--- app/helpers/user_search_queries_helper.rb
+++ app/helpers/user_search_queries_helper.rb
@@ -6,4 +6,18 @@ module UserSearchQueriesHelper
       "was never fetched"
     end
   end
+
+  def format_date date, time_ago = false
+    return '' unless date
+
+    date = Date.parse(date) if date.kind_of? String
+
+    format_string = "%B %eth#{date.kind_of?(Time) ? ', %l:%M %p' : ''}"
+
+    if time_ago && date < Time.now
+      format_string += " (#{time_ago_in_words(date)} ago)"
+    end
+
+    date.strftime format_string
+  end
 end
