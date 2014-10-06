756e7c19b430fb2c7605b371c6631efba21aa5c8
unmanbearpig <unmanbearpig@gmail.com>
2014-10-06 23:54:46 +0400

    Fixed not cleaning database properly after each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,6 +46,18 @@ RSpec.configure do |config|
   config.order = "random"
 
   config.include FactoryGirl::Syntax::Methods
+
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
 
 
