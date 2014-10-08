874bc6953490e15e85ed8f446c7184cc7b565b50
unmanbearpig <unmanbearpig@gmail.com>
2014-10-08 10:06:55 +0400

    fixed deprecated rspec stuff

--- spec/lib/couchsurfing_spec.rb
+++ spec/lib/couchsurfing_spec.rb
@@ -6,31 +6,34 @@ describe CouchSurfing do
 
   context 'valid config' do
     before do
-      CouchSurfing
-        .stub(:credentials) { {'username' => 'user', 'password' => 'pass'} }
+      allow(CouchSurfing)
+        .to receive(:credentials) { {'username' => 'user', 'password' => 'pass'} }
     end
 
 
     it 'passes username and password to new CouchSurfingClient instance' do
-      CouchSurfingClient::CouchSurfing.any_instance.stub(:sign_in)
+      allow_any_instance_of(CouchSurfingClient::CouchSurfing)
+        .to receive(:sign_in)
 
       expect(cs_instance.username).to eq 'user'
       expect(cs_instance.password).to eq 'pass'
     end
 
     it 'signs in' do
-      CouchSurfingClient::CouchSurfing.any_instance
-        .should_receive(:sign_in)
+      expect_any_instance_of(CouchSurfingClient::CouchSurfing)
+        .to receive(:sign_in)
 
-    cs_instance
+      cs_instance
     end
   end
 
   context 'invalid config' do
     it 'raises exception if password is not defined' do
-      CouchSurfing.stub(:fetch_config) { {'username' => 'user'} }
+      allow_any_instance_of(CouchSurfing)
+        .to receive(:fetch_config) { {'username' => 'user'} }
 
-      expect { CouchSurfing.instance }.to raise_error
+      expect { CouchSurfing.instance }
+        .to raise_error
     end
   end
 end
